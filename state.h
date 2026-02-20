8087f4ce1ae3b56a996f9b9f6cabe127cb4f498d
Ivan Mihin <mihin.ivan@outlook.com>
2026-02-20 17:06:56 +0100

    Update state interface
    Add const to handleInput

--- state.h
+++ state.h
@@ -5,7 +5,7 @@ class State
 {
 public:
     virtual ~State() = default;
-    virtual void handleInput(sf::Event& event) = 0;
+    virtual void handleInput(const sf::Event& event) = 0;
     virtual void update(float deltaTime) = 0;
     virtual void render(sf::RenderWindow& window) = 0;
 };
